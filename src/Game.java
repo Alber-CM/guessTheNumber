6bfe9b4287cd1ef70e3ae1c9c4c732ad752d4afd
Alber-CM <albertocarriches@gmail.com>
2025-04-01 21:37:04 +0200

    Finished first version of the game

--- src/Game.java
+++ src/Game.java
@@ -6,7 +6,7 @@ public class Game {
     Random randGen = new Random();
     Scanner guessScan = new Scanner(System.in);
 
-    int randNum, newGuess;
+    int randNum, newGuess, tries;
 
     public Game() {
         randNum = randGen.nextInt(100);
@@ -29,8 +29,30 @@ public class Game {
     }
 
     public void showGuessMenu() {
-        System.out.print("Write your guess: ");
-        newGuess = guessScan.nextInt();
+        for(;;) {
+            tries += 1;
+
+            System.out.print("-> (Press ctrl+c to exit) Write your guess: ");
+            newGuess = guessScan.nextInt();
+            
+            int comp = compareNum(newGuess);
+            
+            if (tries >= 5) {
+                System.out.println("Sorry, you exceed the number of tries (5). Secret number: " + getRandNum());
+                return;
+            }
+
+            if (comp == 0) {
+                System.out.println("\t" + "Congrats, you guess the number!");
+                System.out.println("Number of tries: " + tries);
+                return;
+            }
+            if (comp > 0) {
+                System.out.println("\t" + newGuess + " is lower than secret number.");
+            } else if (comp < 0) {
+                System.out.println("\t" + newGuess + " is higher than secret number.");
+            }
+        }
     }
 
 }
